909161a90104183cd972925b6468bd5ccf77ef2c
Ubuntu <ubuntu@ip-172-31-44-145.us-west-2.compute.internal>
2014-10-05 17:26:08 +0000

    Made some changes to thread behavior in easy_clone.py. Threads lock each other out to rsync - but this is a terrible idea

--- easy_clone.py
+++ easy_clone.py
@@ -11,6 +11,8 @@ hg = []
 git = []
 hg_time = 0
 git_time = 0
+git_lock = threading.Lock()
+hg_lock = threading.Lock()
 
 #This is a generic function that can do the work of either
 #Git or Mercurial, depending on what is passed to it
@@ -18,19 +20,29 @@ def clone(vcs):
     #Specify that we want to use these global variables
     global hg
     global git
+    global git_lock
+    global hg_lock
     total_time = 0
     
     #Get the current time
     begin = time.time()
 
-    #Figure out which list we should look through
+    #Figure out which list we should look through and which locks to use
     if vcs == 'hg':
         my_list = hg
+        my_lock = hg_lock
+        other_lock = git_lock
     else:
         my_list = git
+        my_lock = git_lock
+        other_lock = hg_lock
 
     #Go through every repo in our list and try to clone it
     for i in range(len(my_list)):
+        
+        #Grab this thread's lock. Block here until we can get it
+        my_lock.acquire(True)
+
         #Extract the username and reponame from the full name
         user, repo = my_list[i].split('/')
         #Make a directory for this user. Usernames are unique, so this way,
@@ -50,6 +62,9 @@ def clone(vcs):
         else:
             subprocess.call(['git', 'clone', '--mirror', BITBUCKET+my_list[i]])
         
+        #Release our lock to prevent a deadlock when checking for rsync
+        my_lock.release()
+
         #Check our remaining space. This just calls the shell command 'df'
         #and does some splitting to get the number we want
         space = int(subprocess.check_output(['df', '-k', CLONEHOME+'/'+vcs]).split('\n')[1].split()[3])
@@ -59,12 +74,20 @@ def clone(vcs):
         if space < 10000000:
             #"pause" our timer and add the elapsed time to our total timer
             pause = time.time()
-            total_time += pause - begin
-            #os.system is generally not recommended, but rsync didn't work with subprocess
-            os.system('rsync --remove-source-files -ae "ssh -p 2200" '+CLONEHOME+'/'+vcs+' ahota@da2.eecs.utk.edu:')
-            #The --remove-source-files flag above deletes files but not the directory structure
-            #So we do that separately
-            os.system('rm -r '+CLONEHOME+'/'+vcs+'/*')
+
+            #Block here until this thread can acquire the other thread's lock
+            #The other thread's lock will be free once it finishes cloning
+            other_lock.acquire(True)
+            try:
+                total_time += pause - begin
+                #os.system is generally not recommended, but rsync didn't work with subprocess
+                os.system('rsync --remove-source-files -ae "ssh -p 2200" '+CLONEHOME+'/'+vcs+' ahota@da2.eecs.utk.edu:')
+                #The --remove-source-files flag above deletes files but not the directory structure
+                #So we do that separately
+                os.system('rm -r '+CLONEHOME+'/'+vcs+'/*')
+            finally:
+                #This code will always execute even if something goes wrong above
+                other_lock.release()
             #Get the current time again and continue
             begin = time.time()
     
